8b456f5322148917244d7a7ac6aeba2b348e4087
alingen <kot.90313@gmail.com>
2022-12-15 02:18:53 +0900

    退会処理修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:show]
 
   def withdrawal
-    current_user.update(is_deleted: true)
-    reset_session
-    flash[:notice] = "退会処理を実行いたしました"
-    redirect_to root_path
+    if current_user.update(is_deleted: true)
+      reset_session
+      flash[:notice] = "退会処理を実行いたしました"
+      redirect_to root_path
+    else
+      render user_path
+    end
   end
 
   def show
