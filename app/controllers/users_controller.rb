0c8960511abe1c890153ca16c1290d30104021c7
alingen <kot.90313@gmail.com>
2022-12-15 01:36:07 +0900

    ユーザー詳細画面追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+
+  def withdrawal
+    current_user.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to root_path
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: %i[show] do
+    member do
+      patch :withdrawal
+    end
+  end
 end
