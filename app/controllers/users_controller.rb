ad99de6da9b626630c84ae21175d4ff20b739605
alingen <kot.90313@gmail.com>
2023-03-08 18:12:34 +0900

    アイコン画像を変更可能に

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.image = params[:image]
+    @user.save
+    redirect_to @user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'home#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: %i[show] do
+  resources :users, only: %i[show update] do
     member do
       patch :withdrawal
     end
